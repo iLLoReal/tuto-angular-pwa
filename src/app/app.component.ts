60d0337842a5953105877ab4f582f069e27b0fad
olivier <olivier.laffon.dev@gmail.com>
2020-10-08 12:52:50 +0200

    tuto angular deploy pwa first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
-@Component({
+import {​​​​​ Component }​​​​​ from '@angular/core';
+import {​​​​​ SwPush }​​​​​ from '@angular/service-worker';
+@Component({​​​​​
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'tuto-pwa-angular';
-}
+}​​​​​)
+export class AppComponent {​​​​​
+  readonly VAPID_PUBLIC_KEY = "BAIidyZj45O6wr5W1O2NR9nir_HXR-yBQsRgnm-Z0Un-Gf0wXvUYZ91mpkgplWKVG-IXRAayT-HrKXvTQyRFj9w";
+  title = 'ngseo';
+  constructor(
+    private swPush: SwPush) {​​​​​}​​​​​
+    subscribeToNotifications() {​​​​​
+      this.swPush.requestSubscription({​​​​​
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      }​​​​​)
+      .then(sub => console.log(sub))
+      .catch(err => console.error("Could not subscribe to notifications", err));
+    }​​​​​
+  }​​​​​
+  
