870cf18d073499dd54fbca3d951de31f373efba1
olivier <olivier.laffon.dev@gmail.com>
2020-10-08 14:43:11 +0200

    added console log

--- src/app/newsletter.service.ts
+++ src/app/newsletter.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class NewsletterService {
   constructor(public _http: HttpClient) { }
     addPushSubscriber(sub:any) {
+      console.log("Bonjour");
       return this._http.post('https://www.votrebackendnode.com', sub);
     }
 }
