39c9408733b79d21758e2a400c466f0475db66f8
olivier <olivier.laffon.dev@gmail.com>
2020-10-09 09:45:08 +0200

    removed trailing spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {​​​​​ Component }​​​​​ from '@angular/core';
-import {​​​​​ SwPush }​​​​​ from '@angular/service-worker';
+import {​​​​​ SwPush, SwUpdate }​​​​​ from '@angular/service-worker';
 import { NewsletterService } from './newsletter.service';
 
 @Component({​​​​​
@@ -7,19 +7,24 @@ import { NewsletterService } from './newsletter.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 }​​​​​)
-export class AppComponent {​​​​​
+export class AppComponent {
   readonly VAPID_PUBLIC_KEY = "BAIidyZj45O6wr5W1O2NR9nir_HXR-yBQsRgnm-Z0Un-Gf0wXvUYZ91mpkgplWKVG-IXRAayT-HrKXvTQyRFj9w";
   title = 'tuto-pwa-angular';
+  
   constructor(
-    private swPush: SwPush, private ns:NewsletterService) {​​​​​}​​​​​
-    subscribeToNotifications() {​​​​​
-      this.swPush.requestSubscription({​​​​​
+    private swPush: SwPush, private ns:NewsletterService, public updates:SwUpdate) {
+      this.updates.available.subscribe(event => {
+        alert('update available');
+      });
+    }
+    subscribeToNotifications() {
+      this.swPush.requestSubscription({
         serverPublicKey: this.VAPID_PUBLIC_KEY
-      }​​​​​)
+      })
       .then(sub => this.ns.addPushSubscriber(sub).subscribe( res => {
         console.log(res);
       }))
       .catch(err => console.error("Could not subscribe to notifications", err));
-    }​​​​​
-  }​​​​​
+    }
+  }
   
