492a9d6ca73a238673c275126e05ca39e36ad77e
olivier <olivier.laffon.dev@gmail.com>
2020-10-09 09:38:16 +0200

    modified newsletter

--- src/app/newsletter.service.ts
+++ src/app/newsletter.service.ts
@@ -7,7 +7,6 @@ import { HttpClient } from '@angular/common/http';
 export class NewsletterService {
   constructor(public _http: HttpClient) { }
     addPushSubscriber(sub:any) {
-      console.log("Bonjour");
       return this._http.post('https://tuto-pwa-server.herokuapp.com/subscribe', sub);
     }
 }
